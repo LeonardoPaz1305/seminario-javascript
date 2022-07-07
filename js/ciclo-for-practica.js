e1d9bf177f2c55c1f05b6eacd157751c6db1139d
LeonardoPaz1305 <lavargas@stefanini.com>
2022-07-07 15:58:34 -0500

    practica ciclo for

---
+++ js/ciclo-for-practica.js
@@ -0,0 +1,37 @@
+//1.Realice un programa que enumere los paises de la siguiente lista
+
+//2.Crear un ciclo for que cuente de 0 a 100
+
+//3.Haz una tabla de multiplicar utilizando el ciclo for
+
+//4.Imprima los números del 1 a 10 al revés utilizando el ciclo for
+
+//5.Crear un bucle que cuente todos los números pares hasta el 100
+
+//6.Cree un bucle que sume los números del 100 al 200
+
+//7.Imprima el siguiente patrón con el ciclo for por consola:
+
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+//8.Imprima el siguiente patrón con el ciclo for por consola:
+
+//5 4 3 2 1
+//4 3 2 1
+//3 2 1
+//2 1
+//1
+
+//9.Escriba un código JS para encontrar el número más grande en una matriz 
+// let arr = [2, 45, 3, 67, 34, 567, 34, 345, 123];
+
+//10. Programa para encontrar el número de conteo de ceros en la matriz
+// let arr2 = [[0,1,1],[0,1,0],[1,0,0]];
